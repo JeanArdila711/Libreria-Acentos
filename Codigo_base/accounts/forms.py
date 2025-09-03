56a70d89812e803f963ac9efcebb2071baf3af2d
JeanArdila711 <jardilaa@eafit.edu.co>
2025-09-02 21:22:06 -0500

    Mejorar registro

--- Codigo_base/accounts/forms.py
+++ Codigo_base/accounts/forms.py
@@ -4,7 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=True, label="Nombre")
+    last_name = forms.CharField(max_length=30, required=True, label="Apellido")
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "first_name", "last_name" ,"email", "password1", "password2"]
