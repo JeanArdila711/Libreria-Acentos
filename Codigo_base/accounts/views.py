cafc5b07394368ee6dafee0cd6ce694ee06c9dc8
JeanArdila711 <jardilaa@eafit.edu.co>
2025-09-02 20:06:33 -0500

    Creación de Login, Signup y Logout

---
+++ Codigo_base/accounts/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import SignUpForm
+from django.contrib import messages
+
+# Create your views here.
+
+# Sign Up
+def signup_view(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # inicia sesión automáticamente
+            return redirect("home")  # cámbialo por la ruta de inicio que tengas
+    else:
+        form = SignUpForm()
+    return render(request, "accounts/signup.html", {"form": form})
+
+
+
+# Login
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = AuthenticationForm()
+    return render(request, "accounts/login.html", {"form":form})
+
+# Logout
+def logout_view(request):
+    logout(request)
+    return redirect("/")    
+
+
