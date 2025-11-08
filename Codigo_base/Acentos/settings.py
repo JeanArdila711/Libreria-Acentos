8560479281996e82f78fb7eb590edca4def73318
JeanArdila711 <jardilaa@eafit.edu.co>
2025-11-08 11:55:54 -0500

    Update settings.py

--- Codigo_base/Acentos/settings.py
+++ Codigo_base/Acentos/settings.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Cargar variables desde el archivo .env
-load_dotenv(os.path.join(BASE_DIR, ".env"))
+load_dotenv(os.path.join(BASE_DIR, "openAI.env"))
 
 SECRET_KEY = 'django-insecure-reemplaza-esto-por-una-clave-real'
 DEBUG = True
